1eba69e2dbb064ced8b49e4637edce3813da986e
atharv-lolage <atharvlolage45@gmail.com>
2024-01-16 00:48:13 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -36,7 +36,7 @@ router.post(
       res.json({ user });
     } catch (error) {
       console.error(error.message);
-      res.status(500).json({"Server Error"})
+      res.status(500).send("Server Error");
     }
 
     //   .then(user => res.json(user))
