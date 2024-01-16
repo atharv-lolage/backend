9a5989580babcf3400ffd6533ad4880a5ce83e63
atharv-lolage <atharvlolage45@gmail.com>
2024-01-16 19:05:17 +0530

    user authentication completed

--- routes/auth.js
+++ routes/auth.js
@@ -48,10 +48,10 @@ router.post(
       };
       const authtoken = jwt.sign(data, JWT_SECRET);
       // console.log(jwtData);
-      res.json({authtoken});
+      res.json({ authtoken });
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Server Error");
+      res.status(500).send("Error in Sending response");
     }
 
     //   .then(user => res.json(user))
@@ -59,5 +59,46 @@ router.post(
     // res.json({error: 'Please enter a unique value for email', message: err.message})})
   }
 );
-
+// Create a User using: POST "/api/auth/login". Doesn't require Auth
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    // if there are errors, return bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      // checking whether the user with specific email exists or not
+      let user = await User.findOne({ email });
+      if (!user) {
+        res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+      // checking whether the password given and user password matches or not
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json(authtoken);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Error in Sending response");
+    }
+  }
+);
 module.exports = router;
